f0c243b860fac9a17d9d1fe06f24cc5aa25bb57c
aylwin.cal@gmail.com <aylwin.cal@gmail.com@e279fee4-4e2f-b4d0-517e-b940f31c8a61>
2014-05-05 06:16:54 +0000

    Add http:// to url if it's not there.

--- trunk/check-url/plugin.php
+++ trunk/check-url/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Check URL
 Plugin URI: http://code.google.com/p/yourls-check-url/
 Description: This plugin checks the reachability of an entered URL before creating the short link for it. An error is then returned if the entered URL is unreachable.
-Version: 1.4
+Version: 1.5
 Author: Aylwin
 Author URI: http://adigitalife.net/
 */
@@ -13,62 +13,45 @@ yourls_add_filter( 'shunt_add_new_link', 'churl_reachability' );
 
 // Add a new link in the DB, either with custom keyword, or find one
 function churl_reachability( $churl_reachable, $url, $keyword = '' ) {
-	global $ydb;
+        global $ydb;
 
-	if (function_exists('yourls_get_protocol')){
-		$skip_protocol = check_protocol( $url );
-	} else {
-		preg_match( '!^[a-zA-Z0-9\+\.-]+:(//)?!', $url, $matches );
-		$protocol = ( isset( $matches[0] ) ? $matches[0] : '' );
-		$different_protocols = array (
-			'mailto://',
-			'ftp://',
-			'file://',
-			'telnet://',
-			'ssh://',
-			'sip://'
+        preg_match( '!^[a-zA-Z0-9\+\.-]+:(//)?!', $url, $matches );
+        $protocol = ( isset( $matches[0] ) ? $matches[0] : '' );
+        $different_protocols = array (
+            'http://',
+            'https://'
         );
 
-		$skip_protocol = in_array( $protocol, $different_protocols );
-	}
-	
-	// Return to normal routine if non-http(s) protocol is valid
-	if ($skip_protocol == true){
-		return false;
-	}
+        if ($protocol == '') {
+        	$protocol = 'http://';
+        	$url = 'http://' . $url;
+        }
 
-	// Check if the long URL is reachable	
-	 $resURL = curl_init();
-	curl_setopt($resURL, CURLOPT_URL, $url);
-	curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1);
-	curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback');
-	curl_setopt($resURL, CURLOPT_FAILONERROR, 1);
-	curl_exec ($resURL);
-	$intReturnCode = curl_getinfo($resURL, CURLINFO_HTTP_CODE);
-	curl_close ($resURL);
- 
-	// Return error if the entered URL is unreachable
-	if ($intReturnCode != 200 && $intReturnCode != 302 && $intReturnCode != 304) {
-		$return['status']   = 'fail';
-		$return['code']     = 'error:url';
-		$return['message']  = 'The entered URL is unreachable.  Check the URL or try again later.';
-		$return['statusCode'] = 200; // regardless of result, this is still a valid request
-		return yourls_apply_filter( 'add_new_link_fail_unreachable', $return, $url, $keyword, $title );
-	}
-	
-	return false;
-}
+        $check_url = in_array( $protocol, $different_protocols );
 
-function check_protocol( $url, $protocols = array() ) {
-	if( ! $protocols ) {
-		global $yourls_allowedprotocols;
-		$protocols = $yourls_allowedprotocols;
-	}
-	
-	if ( ( yourls_get_protocol( $url ) == 'http://' ) || ( yourls_get_protocol( $url ) == 'https://' ) ) {
-		return false;
-	} else {
-		$protocol = yourls_get_protocol( $url );
-		return yourls_apply_filter( 'is_allowed_protocol', in_array( $protocol, $protocols ), $url, $protocols );
-	}
+        // Return to normal routine if non-http(s) protocol is valid
+        if ($check_url == false){
+                return false;
+        }
+
+        // Check if the long URL is reachable
+         $resURL = curl_init();
+        curl_setopt($resURL, CURLOPT_URL, $url);
+        curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback');
+        curl_setopt($resURL, CURLOPT_FAILONERROR, 1);
+        curl_exec ($resURL);
+        $intReturnCode = curl_getinfo($resURL, CURLINFO_HTTP_CODE);
+        curl_close ($resURL);
+
+        // Return error if the entered URL is unreachable
+        if ($intReturnCode == '' || $intReturnCode == 404) {
+                $return['status']   = 'fail';
+                $return['code']     = 'error:url';
+                $return['message']  = 'The entered URL is unreachable.  Check the URL or try again later.';
+                $return['statusCode'] = 200; // regardless of result, this is still a valid request
+                return yourls_apply_filter( 'add_new_link_fail_unreachable', $return, $url, $keyword, $title );
+        }
+
+        return false;
 }
